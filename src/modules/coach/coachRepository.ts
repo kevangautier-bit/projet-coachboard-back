d612526b604737b02526f02fad47ab40b9ceeea4
Kevan Gautier <kevangautier@gmail.com>
2026-03-05 21:57:33 +0100

    feat(coach): add coach repository with findAll, findById and create

---
+++ src/modules/coach/coachRepository.ts
@@ -0,0 +1,29 @@
+import type { ResultSetHeader, RowDataPacket } from "mysql2";
+import client from "../../database/client.js";
+
+export const findAll = async () => {
+	const [rows] = await client.query<RowDataPacket[]>("SELECT * FROM COACH");
+	return rows;
+};
+
+export const findById = async (id: string) => {
+	const [rows] = await client.query<RowDataPacket[]>(
+		"SELECT * FROM COACH WHERE ID_COACH = ?",
+		[id],
+	);
+
+	return rows[0] as RowDataPacket | undefined;
+};
+
+export const create = async (
+	prenom: string,
+	nom: string,
+	email: string,
+	motDePasse: string,
+) => {
+	const [result] = await client.query<ResultSetHeader>(
+		"INSERT INTO COACH (PRENOM, NOM, EMAIL, MOT_DE_PASSE) VALUES(?, ?, ?, ?)",
+		[prenom, nom, email, motDePasse],
+	);
+	return result.insertId;
+};
