e2411af6739d16ce97a80311db0087e81c131c44
Kevan Gautier <kevangautier@gmail.com>
2026-03-10 16:03:09 +0100

    feat(coach): add the coachRoutes in the router

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import coachRoutes from "./modules/coach/coachRoutes.js";
 import elevesRoutes from "./modules/eleves/elevesRoutes.js";
 import itemsRoutes from "./modules/items/itemsRoutes.js";
 
@@ -6,5 +7,6 @@ const router = Router();
 
 router.use("/api/items", itemsRoutes);
 router.use("/api/eleves", elevesRoutes);
+router.use("api/coach", coachRoutes);
 
 export default router;
