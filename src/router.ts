2df7dbe6dadaf07dc5e65af4b95942bf86b691bf
Prescillia Rosart <prescillia.rosart@gmail.com>
2026-03-12 11:26:01 +0100

    fix(router): résolution conflit merge avec dev

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import coachRoutes from "./modules/coach/coachRoutes.js";
 import elevesRoutes from "./modules/eleves/elevesRoutes.js";
 import elevesProgrammesRoutes from "./modules/eleves-programmes/elevesprogrammesRoutes.js";
+import exercicesRoutes from "./modules/exercices/exercicesRoutes.js";
 import itemsRoutes from "./modules/items/itemsRoutes.js";
 
 const router = Router();
@@ -10,5 +11,6 @@ router.use("/api/items", itemsRoutes);
 router.use("/api/eleves-programmes", elevesProgrammesRoutes);
 router.use("/api/eleves", elevesRoutes);
 router.use("/api/coach", coachRoutes);
+router.use("/api/exercices", exercicesRoutes);
 
 export default router;
