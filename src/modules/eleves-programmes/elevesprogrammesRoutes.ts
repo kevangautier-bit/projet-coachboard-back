24fa14ffa9b6dbc5e4137e0c065df07d16c3b127
Prescillia Rosart <prescillia.rosart@gmail.com>
2026-03-12 11:07:29 +0100

    feat(elevesprogrammes): ajout des routes et repository elevesprogrammes

---
+++ src/modules/eleves-programmes/elevesprogrammesRoutes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import {
+    create,
+    destroy,
+    getAll,
+    getById,
+    update,
+} from "./elevesprogrammesActions.js";
+
+const router = Router();
+
+router.get("/", getAll);
+router.get("/:id", getById);
+router.post("/", create);
+router.put("/:id", update);
+router.delete("/:id", destroy);
+
+export default router;
--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import coachRoutes from "./modules/coach/coachRoutes.js";
 import elevesRoutes from "./modules/eleves/elevesRoutes.js";
+import elevesProgrammesRoutes from "./modules/eleves-programmes/elevesprogrammesRoutes.js";
 import itemsRoutes from "./modules/items/itemsRoutes.js";
 
 const router = Router();
 
 router.use("/api/items", itemsRoutes);
+router.use("/api/eleves-programmes", elevesProgrammesRoutes);
 router.use("/api/eleves", elevesRoutes);
 router.use("/api/coach", coachRoutes);
 
