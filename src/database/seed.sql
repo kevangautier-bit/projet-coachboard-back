1c0a7966bac5791541a9c182ec9e07422c551af9
Kevan Gautier <kevangautier@gmail.com>
2026-03-17 16:27:38 +0100

    feat: ajout de données fictives dans le seed avec mdp hashés

---
+++ src/database/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO COACH (PRENOM, NOM, EMAIL, MOT_DE_PASSE) VALUES 
+('Julien', 'Marchal', 'julien@coach.com', '$2b$10$/XsCirHvxs4eatv596t2gON1SLelrj0Knrt9dzRNVCZVCONEW4OBm');
+
+INSERT INTO ELEVES (PRENOM, NOM, EMAIL, MOT_DE_PASSE, AGE, POIDS_INITIAL, TAILLE, OBJECTIF, NIVEAU) VALUES
+('Kevan', 'Gautier', 'kevangautier@gmail.com', '$2b$10$Xv4ppLQbeY84vWjnEM1dNuciqFS63QnsM1i/KVycvDcbxENrsNCK6', 32, 92.00, 1.90, 'prise de masse', 'débutant' );
